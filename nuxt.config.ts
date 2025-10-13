d9ef2e6f3fd597fdaafe06bce39a160d24ad5569
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-10-13 13:18:11 +0200

    Forces light color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
     devtools: { enabled: false },
     modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/i18n', '@pinia/nuxt'],
     css: ['~/assets/css/main.css'],
+    colorMode: {
+        preference: 'light',
+    },
 });
