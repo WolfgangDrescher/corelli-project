5438ea18782769e3fb36a7ab751236d743266007
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-10-13 13:17:17 +0200

    npx nuxi@latest module add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: false },
-  modules: [
-    '@nuxt/content',
-    '@nuxt/ui',
-    '@nuxtjs/i18n',
-  ],
-  css: ['~/assets/css/main.css'],
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: false },
+    modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/i18n', '@pinia/nuxt'],
+    css: ['~/assets/css/main.css'],
 });
