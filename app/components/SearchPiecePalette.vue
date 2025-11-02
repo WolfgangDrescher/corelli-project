e0a5d0a192992ccfaab820a36ee3b7140bde7bdd
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-11-02 23:17:05 +0100

    Add SearchPiecePalette component

---
+++ app/components/SearchPiecePalette.vue
@@ -0,0 +1,96 @@
+<script setup>
+const { data: pieces } = await useAsyncData(`pieces`, () => queryCollection('pieces').all());
+
+const open = defineModel('open', { type: Boolean })
+
+const searchTerm = ref('');
+
+const localePath = useLocalePath();
+
+function fuzzysearch (needle, haystack) {
+    needle = needle.toLowerCase();
+    haystack = haystack.toLowerCase();
+    var hlen = haystack.length;
+    var nlen = needle.length;
+    if (nlen > hlen) {
+        return false;
+    }
+    if (nlen === hlen) {
+        return needle === haystack;
+    }
+    outer: for (var i = 0, j = 0; i < nlen; i++) {
+        var nch = needle.charCodeAt(i);
+        while (j < hlen) {
+            if (haystack.charCodeAt(j++) === nch) {
+                continue outer;
+            }
+        }
+        return false;
+    }
+    return true;
+}
+
+const groups = computed(() => {
+    if (!pieces.value) return []
+    
+    const groupedPieces = Object.groupBy(pieces.value, p => `${p.op}-${p.nr}`)
+    
+    const commandGroups = Object.entries(groupedPieces).map(([key, groupPieces]) => {
+        const { op, nr } = groupPieces[0]
+        
+        const filteredItems = groupPieces.map(p => {
+            const title = p.title?.toLowerCase() !== p.slug?.toLowerCase() ? p.title : '';
+            const opStr = String(p.op).padStart(2, '0');
+            const nrStr = String(p.nr).padStart(2, '0');
+            return {
+                label: `${p.mv}. ${p.movementDesignation}${title ? ` (${title})` : ''}`,
+                value: p.slug,
+                search: `opus. ${opStr} no nr ${nrStr} ${p.mv}. ${title} ${p.movementDesignation}`,
+                onSelect: () => {
+                    navigateTo(localePath({ name: 'piece-id', params: { id: p.slug } }));
+                    open.value = false;
+
+                },
+            };
+        }).filter(item => fuzzysearch(searchTerm.value, item.value) || fuzzysearch(searchTerm.value, item.search))
+        
+        return {
+            id: `op${op}n${nr}`,
+            label: `Op. ${op} №${nr}`,
+            ignoreFilter: true,
+            items: filteredItems,
+        }
+    })
+    
+    // Leere Gruppen ausfiltern
+    return commandGroups.filter(g => g.items.length > 0)
+});
+</script>
+
+<template>
+    <UModal v-model:open="open">
+        <UButton
+            :label="$t('search')"
+            color="neutral"
+            variant="subtle"
+            icon="i-lucide-search"
+        >
+            <template #trailing>
+                <UKbd value="meta" />
+                <UKbd color="neutral">K</UKbd>
+            </template>
+        </UButton>
+        
+        <template #content>
+            <UCommandPalette
+                :groups="groups"
+                :placeholder="$t('searchPieces')"
+                v-model:search-term="searchTerm"
+            >
+                <template #item-trailing="{ item }">
+                    <div class="font-mono text-[0.75rem] text-gray-500 translate-y-0.5">{{ item.value }}</div>
+                </template>
+            </UCommandPalette>
+        </template>
+    </UModal>
+</template>
