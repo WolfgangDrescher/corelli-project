bc39ee98b001bbe2eed24c69f046f4ad06df7223
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-11-05 13:53:49 +0100

    Add about page

---
+++ app/pages/about.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { t } = useI18n();
+useHead({
+    title: t('about'),
+});
+</script>
+
+<template>
+    <UContainer>
+        <section class="flex flex-col gap-8">
+            <Heading>{{ $t('about') }}</Heading>
+        </section>
+    </UContainer>
+</template>
