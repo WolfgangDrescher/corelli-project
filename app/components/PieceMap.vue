8fbdfe7b974c919a5d770cf4cff03163f1c036d4
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-12-18 09:26:00 +0100

    Improve key box width

--- app/components/PieceMap.vue
+++ app/components/PieceMap.vue
@@ -45,8 +45,10 @@ function getLeft(startBeat) {
                         sideOffset: 8
                     }"
                 >
-                    <div class="w-full h-full bg-primary/20 border-primary/40 border rounded px-2 flex items-center hover:bg-primary/50 hover:border-primary/60 hover:shadow">
-                        {{ group[0] }}
+                    <div class="w-full h-full bg-primary/20 border-primary/40 border rounded flex items-center hover:bg-primary/50 hover:border-primary/60 hover:shadow">
+                        <div class="px-2">
+                            {{ group[0] }}
+                        </div>
                     </div>
                 </UTooltip>
             </div>
